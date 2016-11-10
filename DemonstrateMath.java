a3aa737d37602e709e2ad00f8ea3fcc8d474c02f
Tapnish Wadhwa <tapnish@Hotmail.com>
2016-11-10 09:04:37 -0500

    Add files via upload

---
+++ DemonstrateMath.java
@@ -0,0 +1,21 @@
+public class DemonstrateMath {
+   public static void main (String[] args)
+   {
+System.out.println("a. Math.abs(-5) = " + Math.abs(-5));
+System.out.println("b. Math.abs(5) = " + Math.abs(5));
+System.out.println("c. (Math.abs(-10) - Math.abs(6)) = " + (Math.abs(-10) - Math.abs(6)));
+System.out.println("d. Math.pow(4,2) = " + Math.pow(4,2));
+System.out.println("e. Math.pow(2,4) = " + Math.pow(2,4));
+System.out.println("f. Math.pow(3,3) = " + Math.pow(3,3));
+System.out.println("g. Math.sqrt(81) = " + Math.sqrt(81));
+System.out.println("h. Math.sqrt(0.0064) = " + Math.sqrt(0.0064));
+System.out.println("i. Math.sqrt(100) = " + Math.sqrt(100));
+System.out.println("j. Math.round(3.499) = " + Math.round(3.499));
+System.out.println("k. Math.round(3.501) = " + Math.round(3.501));
+System.out.println("l. Math.ceil(4.002) = " +Math.ceil(4.002));
+System.out.println("m. Math.floor(7.99) = " + Math.floor(7.99));
+System.out.println("o. Math.min(100,2) = " + Math.min(100,2));
+System.out.println("p. Math.max(100,2) = " +Math.max(100,2));
+System.out.println("q. Math.PI = " + Math.PI);
+}
+} 
