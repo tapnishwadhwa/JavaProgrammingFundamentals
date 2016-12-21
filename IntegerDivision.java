a31dc48d13e2c0bda9b0f2deafe9ed619bd4632c
Tapnish Wadhwa <tapnish@Hotmail.com>
2016-12-21 09:04:42 -0500

    Add files via upload

---
+++ IntegerDivision.java
@@ -0,0 +1,10 @@
+class IntegerDivision
+{
+  public static void main ( String[] args )
+   {
+     System.out.println("13/4 = " + (13/4) );
+     System.out.println("13.0/4 = " + (13.0/4) );
+     System.out.println("13/4.0 = " + (13/4.0) );
+     System.out.println("13.0/4.0 = " + (13.0/4.0) );
+   }
+}
